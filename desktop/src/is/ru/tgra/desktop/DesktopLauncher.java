da41fe637a09f72d5a95b67b713efa8906686493
Hlynur Stefánsson <hlynurs15@ru.is>
2017-09-01 21:24:48 +0000

    initial commit

---
+++ desktop/src/is/ru/tgra/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package is.ru.tgra.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import is.ru.tgra.Breakout;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Breakout(), config);
+	}
+}
